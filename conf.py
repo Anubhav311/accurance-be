223a76cca86f06d2fbfad1555586fb936d4778fd
Marve <anubhavbhambri1@gmail.com>
2026-02-10 17:00:23 +0530

    changed conf.py

--- conf.py
+++ conf.py
@@ -1,11 +1,21 @@
-from pydantic_settings import BaseSettings
-from dotenv import load_dotenv
-import os
-
-load_dotenv()  # Load environment variables from .env file
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import Field
 
 class Settings(BaseSettings):
-    origin_localhost: str = os.getenv("ORIGIN_HOST")
-    origin_localhost_2: str = os.getenv("ORIGIN_HOST_2")
+    cors_origins: str = Field(..., env="CORS_ORIGINS")
+
+    @property
+    def origins(self) -> list[str]:
+        return [
+            origin.strip()
+            for origin in self.cors_origins.split(",")
+            if origin.strip()
+        ]
+    
+    model_config = SettingsConfigDict(
+        env_file=".env",               # local dev only
+        env_file_encoding="utf-8",
+        case_sensitive=True
+    )
 
 settings = Settings()
